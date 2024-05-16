340f91aa17b2c3568377a40e15881569745bd83b
Kris Urbas <kris@aave.com>
2024-05-16 12:56:19 +0200

    restructure

--- template.config.js
+++ template.config.js
@@ -2,13 +2,12 @@
  * Init a new React Native project with the Lens template
  *
  * ```bash
- * $ npx @react-native-community/cli init LensApp --template https://github.com/lens-protocol/lens-sdk/examples/react-native-template
- * $ npx @react-native-community/cli init LensApp --template https://github.com/lens-protocol/lens-sdk/tree/react-native-template/examples/react-native-template
+ * $ npx @react-native-community/cli init LensApp --template https://github.com/krzysu/lens-react-native-template
  * ```
  * */
 
 module.exports = {
   placeholderName: "HelloWorld",
   titlePlaceholder: "Hello App Display Name",
-  templateDir: ".",
+  templateDir: "./template",
 };
